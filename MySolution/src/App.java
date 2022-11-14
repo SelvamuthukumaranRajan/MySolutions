f67732dffa3b8f3da6ba9ccbaff081109536d746
Selvamuthukumaran Rajan <76645329+SelvamuthukumaranRajan@users.noreply.github.com>
2022-11-14 19:25:30 +0530

    Added few linked list problems

--- MySolution/src/App.java
+++ MySolution/src/App.java
@@ -5,6 +5,9 @@ import tree.*;
 public class App {
     public static void main(String[] args) throws Exception {
         Node head = new Node(1, null);
+        // head = new Node(2, head);
+        // head = new Node(2, head);
+        // head = new Node(1, head);
 
         int i = 2;
         while(i<8){
@@ -15,7 +18,11 @@ public class App {
         var linkedListObj = new LinkedListProbs();
         System.out.println("Given linked list : "+linkedListObj.linkedListToString(head));
         System.out.println("Fourth element from end of linked list : "+linkedListObj.nthElementFromLast(head, 4));
+        System.out.println("Midddle element of linked list Approach1: "+linkedListObj.middleElementOfLinkedList(head));
+        System.out.println("Midddle element of linked list Approach2: "+linkedListObj.middleElementOfLinkedListUsingPointer(head));
+        System.out.println("Is palindrome linked list : "+linkedListObj.isPalindromeLikedList(head));
         System.out.println("Reversed linked list : "+linkedListObj.linkedListToString(linkedListObj.reverseLinkedList(head)));
+        System.out.println("Given linked list : "+linkedListObj.linkedListToString(head));
         System.out.println("Is linked list have cycle : "+linkedListObj.linkedListHasCycle(head));
 
         Tree subtree2Left1 = new Tree(1,null,null);
@@ -26,15 +33,15 @@ public class App {
         Tree subtree1Right = new Tree(7,subtree2Right1,subtree2Right2);
         Tree treeHead = new Tree(5,subtree1Left,subtree1Right);
 
-        System.out.println("Trees");
-        var treeObj = new TreeProbs();
-        System.out.println("Nth distance nodes:"+treeObj.getNthDistanceNode(treeHead, 0).toString());
-        System.out.println("Height of tree:"+treeObj.heightOfTheTree(treeHead));
-        System.out.println("Pre Order:"+treeObj.treePreOrder(treeHead).toString());
-        System.out.println("Post Order:"+treeObj.treePostOrder(treeHead).toString());
-        System.out.println("In Order:"+treeObj.treeInOrder(treeHead).toString());
-        System.out.println("Level Order:"+treeObj.treeLevelOrder(treeHead).toString());
-        System.out.println("IsBST :"+treeObj.isBST(treeHead));
+        // System.out.println("Trees");
+        // var treeObj = new TreeProbs();
+        // System.out.println("Nth distance nodes:"+treeObj.getNthDistanceNode(treeHead, 0).toString());
+        // System.out.println("Height of tree:"+treeObj.heightOfTheTree(treeHead));
+        // System.out.println("Pre Order:"+treeObj.treePreOrder(treeHead).toString());
+        // System.out.println("Post Order:"+treeObj.treePostOrder(treeHead).toString());
+        // System.out.println("In Order:"+treeObj.treeInOrder(treeHead).toString());
+        // System.out.println("Level Order:"+treeObj.treeLevelOrder(treeHead).toString());
+        // System.out.println("IsBST :"+treeObj.isBST(treeHead));
 
     }
 }
--- MySolution/src/linkedlist/LinkedListProbs.java
+++ MySolution/src/linkedlist/LinkedListProbs.java
@@ -2,6 +2,7 @@ package linkedlist;
 
 public class LinkedListProbs {
    
+    //To find nth element of linked list
     public int nthElementFromLast(Node head, int n){
         Node left = head;
         Node right = head;
@@ -21,9 +22,14 @@ public class LinkedListProbs {
         return left.value;
     }
 
+    //To reverse linked list
     public Node reverseLinkedList(Node head){
-        Node current = head;
-        Node result = null;
+        if(head == null || head.child == null){
+            return head;
+        }
+        
+        Node current = head.child;
+        Node result = head;
 
         while(current!=null){
             Node nextChild = current.child;
@@ -31,10 +37,10 @@ public class LinkedListProbs {
             result = current;
             current = nextChild;
         }
-
         return result;
     }
 
+    //To detect linked list cycle
     public boolean linkedListHasCycle(Node head){
         Node fast = head;
         Node slow = head;
@@ -49,6 +55,50 @@ public class LinkedListProbs {
         return false;
     }
 
+    //Middle element of the LinkedList
+    //Approach 1
+    public int middleElementOfLinkedList(Node head){
+        Node current = head;
+        int length = lengthOfLinkedList(head);
+        for(int i=0; i<length/2; i++){
+            current = current.child;
+        }
+        return current.value;
+    }
+    //Approach 2
+    public int middleElementOfLinkedListUsingPointer(Node head){
+        Node slow = head;
+        Node fast = head;
+        while(fast.child != null && fast.child.child != null){
+            slow = slow.child;
+            fast = fast.child.child;
+        }
+        return slow.value;
+    }
+
+    //Lenght of linked list
+    public int lengthOfLinkedList(Node head){
+        Node current = head;
+        int result = 1;
+        while(current.child!=null){
+            current = current.child;
+            result++;
+        }
+        return result;
+    }
+
+    //To check palindrome 
+    public boolean isPalindromeLikedList(Node head){
+        Node original = head;
+        Node reversed = reverseLinkedList(head);
+
+        System.out.println("Original:"+linkedListToString(original));
+        System.out.println("Reversed:"+linkedListToString(reversed));
+        return original==reversed;
+    }
+
+
+    //Linked list to string
     public String linkedListToString(Node head){
         StringBuilder result = new StringBuilder();
 
